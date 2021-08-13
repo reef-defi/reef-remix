5448f59c422381afbf4645b0349666c95f4971a9
Žiga Franko <franko.ziga@gmail.com>
2021-08-13 13:46:35 +0200

    reef plugin host added

--- apps/remix-ide/src/app.js
+++ apps/remix-ide/src/app.js
@@ -214,7 +214,7 @@ async function run () {
     toolTip('You are using an `https` connection. Please switch to `http` if you are using Remix against an `http Web3 provider` or allow Mixed Content in your browser.')
   }
 
-  const hosts = ['127.0.0.1:8080', '192.168.0.101:8080', 'localhost:8080']
+  const hosts = ['127.0.0.1:8080', '192.168.0.101:8080', 'localhost:8080', 'https://remix-plugin.reefscan.com/']
   // workaround for Electron support
   if (!isElectron() && !hosts.includes(window.location.host)) {
     // Oops! Accidentally trigger refresh or bookmark.
